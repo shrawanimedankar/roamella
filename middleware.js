b1e25699886f663b3a89798ba2f7b7c98fd675c6
Shrawani Medankar <shrawanimedankar@gmail.com>
2026-01-22 21:24:07 +0530

    Inial Commit

--- middleware.js
+++ middleware.js
@@ -25,7 +25,6 @@ module.exports.validateReview = (req, res, next) => {
   }
 };
 
-
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     req.session.redirectUrl = req.originalUrl;
@@ -41,21 +40,41 @@ module.exports.saveRedirectUrl = (req, res, next) => {
   next();
 };
 
+
 module.exports.isOwner = async (req, res, next) => {
+  if (!res.locals.currUser) {
+    req.flash("error", "You must be logged in");
+    return res.redirect("/login");
+  }
   let { id } = req.params;
   let listing = await Listing.findById(id);
+  if (!listing) {
+    req.flash("error", "Listing not found");
+    return res.redirect("/listings");
+  }
   if (!listing.owner.equals(res.locals.currUser._id)) {
-    req.flash("error", "No Permission, you are not the owner");
+    req.flash("error", "No permission, you are not the owner");
     return res.redirect(`/listings/${id}`);
   }
   next();
 };
 
+
+
+
 module.exports.isReviewAuthor = async (req, res, next) => {
+  if (!res.locals.currUser) {
+    req.flash("error", "You must be logged in");
+    return res.redirect("/login");
+  }
   let { id, reviewId } = req.params;
   let review = await Review.findById(reviewId);
+  if (!review) {
+    req.flash("error", "Review not found");
+    return res.redirect(`/listings/${id}`);
+  }
   if (!review.author.equals(res.locals.currUser._id)) {
-    req.flash("error", "No Permission, you are not the author of this review");
+    req.flash("error", "No permission, you are not the author");
     return res.redirect(`/listings/${id}`);
   }
   next();
